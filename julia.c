e5ff4780e0b8b0c956ba88ff8f20d45293fb5a65
Denis Semenov <dsemenov@student.42lyon.fr>
2025-03-06 15:24:49 +0100

    Add julia.c. Change fractol.c name to mandelbrot.c

---
+++ julia.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   julia.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsemenov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 18:17:22 by dsemenov          #+#    #+#             */
+/*   Updated: 2025/03/06 15:20:25 by dsemenov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include "fractol.h"
+
+t_complex	print_sequence(int n, t_complex c, t_complex z0)
+{
+	int	i;
+	double	tmp_real;
+	double	tmp_i;
+	t_complex	zn;
+
+	zn = z0;
+	i = 0;
+	while (i < n)
+	{
+		tmp_real = (zn.real * zn.real) - (zn.i * zn.i);
+		tmp_i = 2 * zn.real * zn.i;
+		zn.real = tmp_real + c.real;
+		zn.i = tmp_i + c.i;
+		printf("z%d:\nReal part: %f\nImaginary part: %f\n\n", i + 1, zn.real, zn.i);
+		i++;
+	}
+	return (zn);
+}
+
+// 1 argument: C real part, 2 arg: C imaginary part, 3 argument: z0 real part, 4 arg: z0 imaginary part, 5 arg: number of iterations
+int	main(int argc, char **argv)
+{
+	t_complex	c;
+	t_complex	z0;
+	t_complex	res;
+	int	n;
+
+	if (argc != 6)
+	{
+		printf("Enter arguments:\n1.C real part 2.C imaginary part 3.z0 real part 4.z0 imaginary part 5.Number of iterations\n");
+    	printf("Example: ./a.out -0.8 0.156 0.2 0 15\n");
+    return (1);
+		return (0);
+	}
+	c.real = atof(argv[1]);
+	c.i = atof(argv[2]);
+	z0.real = atof(argv[3]);
+	z0.i = atof(argv[4]);
+	n = atoi(argv[5]);
+	res = print_sequence(n, c, z0);
+	if (res.real * res.real + res.i * res.i > 4)
+		printf("\nz0 doesn't relate to Julia set\n");
+	else
+		printf("\nz0 relates to Julia set\n");
+	return (0);
+}
